c9d9c1509d5fd67f2558bb96a896fb264226cd34
uunknown 13 <shubhamchamar>
2024-10-12 09:57:51 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// Serve the static files from "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Serve the main page
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+// Start the server on port 3000
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
